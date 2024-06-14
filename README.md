33991d0ae6540a2992b90242ea7fe9a73dc8da16
Destan Sarpkaya <destan@destan.dev>
2024-06-14 13:56:01 +0300

    initial

---
+++ README.md
@@ -0,0 +1,22 @@
+
+### Plugin Manifest
+
+```json
+{
+  "title": "Showcase",
+  "src": "https://e6fa-2a02-4e0-2d7d-c8d-5883-39e8-b0d6-c31d.ngrok-free.app/",
+  "uiDefinition": {
+    "height": 900
+  },
+  "singleton": false
+}
+```
+
+### Plugin Settings
+
+```json
+{
+  "bg-color": "orange",
+  "text-color": "white"
+}
+```
---
+++ plugin-implementation.js
@@ -0,0 +1,70 @@
+const LABEL = 'Grispi Plugin Showcase';
+const GRISPI_API_URL = 'https://localhost:8443';
+const grispiClient = GrispiClient.instance();
+let isNewTicket = false;
+
+grispiClient.init().then(bundle => {
+  log('Plugin initialized.');
+  console.log(LABEL, bundle);
+
+  const context = bundle.context;
+  const pluginSettings = bundle.settings;
+
+  // If the app is loaded on a "New Ticket" tab then the ticket object in the context object (context.ticket) is null
+  const ticket = context.ticket;
+  isNewTicket = ticket == null;
+
+  updateUiWithContext(context);
+  updateUiWithPluginSettings(pluginSettings);
+
+  if (!isNewTicket) {
+    const upToDateTicketFromServer = getTicket(ticket.key, context.token, context.tenantId);
+
+    // TODO UI initialization with ticket and requester info
+  }
+});
+
+grispiClient.activeTicketChanged = function(ticketKey) {
+  // Intentionally left blank
+}
+
+grispiClient.currentTicketUpdated = function(ticketKey) {
+  const ticket = getTicket('TICKET-1', context.token, context.tenantId);
+
+  // TODO UI update with updated ticket info
+}
+
+function updateUiWithContext(context) {
+  if (isNewTicket) return;
+
+  document.querySelector('input[name=ticket-key]').value = context.ticketKey;
+  document.querySelector('textarea[name=ticket-fields]').value = JSON.stringify(context.ticket.fieldMap, null, 2);
+  // TODO further UI updates...
+}
+
+function updateUiWithPluginSettings(pluginSettings) {
+  document.body.style.backgroundColor = pluginSettings.bgColor;
+  document.body.style.color = pluginSettings.textColor;
+}
+
+function log(message) {
+  document.getElementById('log').innerText += `\n${new Date().toLocaleTimeString()} ${message}`;
+}
+
+async function getTicket(ticketKey, token, tenantId) {
+
+  // See https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#supplying_your_own_request_object
+  const response = await fetch(`${GRISPI_API_URL}/public/v1/tickets/${ticketKey}`, {
+    method: "GET",
+    cache: "no-cache",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`,
+      "tenantId": tenantId
+    }
+  });
+  // TODO error handling
+  const ticketJson = response.json();
+  console.log(LABEL, 'ticket', ticketJson);
+  return response.json();
+}
